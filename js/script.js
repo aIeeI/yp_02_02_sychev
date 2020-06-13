8cac8e04608e29d6a6c875e21ddf575cc2dff5f9
aIeeI <66679706+aIeeI@users.noreply.github.com>
2020-06-13 12:17:59 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+	$(document).ready(function(){
+    
+  $(".up").hide(); 
+  $(".clickMenu").click(function(){
+   
+    $(".visibleMenu").toggle(500);
+      
+     $(".up").toggle(); 
+     $(".down").toggle();
+  });  
+});
